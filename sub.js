233df4b6168fcae337315ab4fe9adf8e86d397a7
ajunglee <124266040+ajunglee@users.noreply.github.com>
2023-02-09 22:54:51 +0900

    Add files via upload

---
+++ sub.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.menu-trigger').click(function(){
+        $(this).toggleClass('active');
+        $('.m_menu').slideToggle(400);
+    });
+    
+    
+    
+});
